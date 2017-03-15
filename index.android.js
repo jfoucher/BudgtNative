b6976901fe4989b8aed83a214de3788afe3bbfa3
jfoucher <jfoucher@6px.eu>
2017-03-15 10:43:49 +0100

    Add theme to android

--- index.android.js
+++ index.android.js
@@ -13,10 +13,45 @@ import {
 } from 'react-native';
 import App from './components/App'
 
+import { COLOR, ThemeProvider } from 'react-native-material-ui';
+
+
+const uiTheme = {
+  palette: {
+    primaryColor: COLOR.teal500,
+    primary2Color: COLOR.teal700,
+    accentColor: COLOR.orange500,
+  },
+  toolbar: {
+    container: {
+      height: 50,
+    },
+  },
+  drawerHeader: {
+    contentContainer: {
+      backgroundColor: COLOR.teal500,
+    }
+  },
+  drawerHeaderListItem: {
+    secondaryText: {
+      color: '#FFF'
+    },
+    primaryText: {
+      color: '#FFF'
+    },
+    rightElement: {
+      color: '#FFF'
+    }
+  }
+};
+
+
 export default class BudgtNative extends Component {
   render() {
     return (
-      <App />
+        <ThemeProvider uiTheme={uiTheme}>
+          <App />
+        </ThemeProvider>
     );
   }
 }
